dcc810f5b92891cabecd2abdf053dd061200817b
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-03-15 08:55:30 -0300

    Cria rotas

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,40 @@
+import config from './config.js';
+import { Controller } from './controller.js';
+import { logger } from './util.js';
+const controller = new Controller()
+const homeHTML = config.pages.homeHTML
+const controllerHTML = config.pages.controllerHTML
+
+
+async function routes(request, response){
+  const { method, url } = request
+
+  if(method == 'GET' && url == '/'){
+    response.writeHead(302, {
+      'Location': config.location.home
+    })
+
+    return response.end()
+  }
+
+  if(method == 'GET' && url == '/home'){
+    const {
+      stream
+    } = await controller.getFileStream(homeHTML)
+
+    return stream.pipe(response)
+  }
+
+  if(method == 'GET' && url == '/controller'){ // https://youtu.be/_yolPPGtySM?t=3572
+    const {
+      stream
+    } = await controller.getFileStream(controllerHTML)
+
+    return stream.pipe(response)
+  }
+}
+
 export function handler(request, response){
-  return response.end('hello world')
+  return routes(request, response)
+  .catch(error => logger.error(error.stack))
 }
