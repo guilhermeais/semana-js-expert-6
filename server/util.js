cb09b93188b2fd93aa19271838ed238c8e2c7b48
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-03-17 22:50:38 -0300

    Arruma log_disabled do pino

--- server/util.js
+++ server/util.js
@@ -1,7 +1,7 @@
 import pino from 'pino';
-
+const LOG_DISABLED = process.env.LOG_DISABLED === 'true';
 const log = pino({
-  enabled: !(!!process.env.LOG_DISABLED),
+  enabled: !LOG_DISABLED,
   transport: {
     target: 'pino-pretty',
     options: {
