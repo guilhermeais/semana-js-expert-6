76a25f61ed14b9b45c3ccb15d76a06c5ff269120
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-03-20 13:37:17 -0300

    utiliza sox para obter o bitrate

--- server/service.js
+++ server/service.js
@@ -4,8 +4,12 @@ import fsPromises from 'fs/promises'
 import { join, extname } from 'path'
 import { PassThrough } from 'stream'
 import config from './config.js'
+import throttle from 'throttle'
+import childProcess from 'child_process'
+import { logger } from './util.js'
 
 const publicDirectory = config.dir.publicDirectory
+const fallbackBitRate = config.constants.fallbackBitRate
 export default class Service {
   constructor(){
     this.clientStreams = new Map();
@@ -24,6 +28,37 @@ export default class Service {
 
   removeClientStream(id){
     this.clientStreams.delete(id)
+  }
+
+  _executeSoxCommand(args) {
+    return childProcess.spawn('sox', args)
+  }
+
+  async getBitRate(song) {
+    try {
+      const args = [
+        '--i', // info
+        '-B', // bitrate
+        song
+      ]
+
+      const {
+        stderr, // tudo o que é erro
+        stdout, // tudo o que é log
+        // stdin // enviar dados como string
+       } = this._executeSoxCommand(args)
+
+       const [success, error] = [stdout, stderr].map(stream => stream.read())
+       if(error) return await Promise.reject(error);
+       
+       return success
+       .toString()
+       .trim()
+       .replace(/k/, '000') // k -> 000
+    } catch (error) {
+      logger.error(`error on getBitRate`,error)
+      return fallbackBitRate
+    }
   }
 
   createFileStream(filename){
