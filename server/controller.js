7f07e488aef482e035e9dc7d52644a8bdde74b48
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-03-21 20:17:06 -0300

    Adiciona método para lidar com comandos de start e stop

--- server/controller.js
+++ server/controller.js
@@ -10,6 +10,23 @@ export default class Controller {
     return this.service.getFileStream(filename)
   }
 
+  async handleCommand({ command }){
+    logger.info(`handling command ${command}`)
+    const result = {
+      message: 'ok'
+    }
+    const cmd = command.toLowerCase()
+    if(cmd.includes('start')){
+      this.service.startStreaming()
+      return result
+    }
+
+    if(cmd.includes('stop')){
+      this.service.stopStreaming()
+      return result
+    }
+  }
+
   createClientStream() {
     const { id, clientStream } = this.service.createClientStream()
 
