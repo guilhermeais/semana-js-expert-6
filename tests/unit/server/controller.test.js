4764a126ebe3f472af8ce7c8d3dcb3dca94b22e0
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-03-26 10:24:47 -0300

    Cria mocks e testa o handleCommand

--- tests/unit/server/controller.test.js
+++ tests/unit/server/controller.test.js
@@ -3,6 +3,25 @@ import  Service  from '../../../server/service';
 import Controller from '../../../server/controller'
 import TestUtil from '../_util/testUtil';
 
+class SpyService {
+  constructor(){
+    this.startStreaming = jest.fn()
+    this.stopStreaming = jest.fn()
+    this.createClientStream = jest.fn()
+    this.removeClientStream = jest.fn()
+  }
+}
+
+function makeController(){
+  const spyService = new SpyService()
+  const controller = new Controller()
+  controller.service = spyService
+  return {
+    controller,
+    spyService
+  }
+}
+
 describe('#Controller - test suite for controller return', () => {
   beforeEach(() => {
     jest.restoreAllMocks()
@@ -30,5 +49,63 @@ describe('#Controller - test suite for controller return', () => {
         type: mockFileInfo.type
       })
     })
+  });
+
+  describe('handleCommand', () => {
+    test('should call service.startStreaming when command start was passed', async () => {
+      const {controller, spyService} = makeController()
+
+      jest.spyOn(String.prototype, String.prototype.toLowerCase.name)
+      jest.spyOn(String.prototype, String.prototype.includes.name)
+     
+     const commandString = new String('start')
+      const commandParamMock = {
+        command: commandString
+      }
+
+      const result = await controller.handleCommand(commandParamMock)
+      expect(commandString.toLowerCase).toHaveBeenCalled()
+      expect(commandString.includes).toHaveBeenCalledWith('start')
+      expect(spyService.startStreaming).toHaveBeenCalled()
+      expect(result).toStrictEqual({
+        message: 'ok'
+      })
+    });
+
+    test('should call stopStreaming when command stop was provided', async () => {
+      const {controller, spyService} = makeController()
+
+      jest.spyOn(String.prototype, String.prototype.toLowerCase.name)
+      jest.spyOn(String.prototype, String.prototype.includes.name)
+      
+      const commandString = new String('stop')
+       const commandParamMock = {
+         command: commandString
+       }
+ 
+       const result = await controller.handleCommand(commandParamMock)
+       expect(commandString.toLowerCase).toHaveBeenCalled()
+       expect(commandString.includes).toHaveBeenCalledWith('stop')
+       expect(spyService.stopStreaming).toHaveBeenCalled()
+       expect(result).toStrictEqual({
+         message: 'ok'
+       })
+    });
+    test('should do nothing if command is an empty string', async () => {
+      const {controller, spyService} = makeController()
+
+      jest.spyOn(String.prototype, String.prototype.toLowerCase.name)
+      jest.spyOn(String.prototype, String.prototype.includes.name)
+      
+      const commandString = new String('')
+       const commandParamMock = {
+         command: commandString
+       }
+ 
+       const result = await controller.handleCommand(commandParamMock)
+       expect(commandString.toLowerCase).toHaveBeenCalled()
+ 
+       expect(result).toBeUndefined()
+    });
   });
 });
