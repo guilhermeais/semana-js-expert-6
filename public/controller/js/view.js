5ce7806bf62f5f4d50a31c6257bb9207cb93bd9a
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-04-04 08:30:16 -0300

    Cria método para mudar a visibilidade dos botões do controlador

--- public/controller/js/view.js
+++ public/controller/js/view.js
@@ -1,3 +1,16 @@
 export default class View {
+  onLoad(){
+    this.changeCommandButtonsVisibility();
+  }
 
+  changeCommandButtonsVisibility(hide = true){
+    Array.from(document.querySelectorAll('[name=command]'))
+    .forEach(button => {
+      const fn = hide ? 'add' : 'remove'
+      button.classList[fn]('unassigned')
+     
+      function onClickReset(){}
+      button.onclick = onClickReset 
+    })
+  }
 }
