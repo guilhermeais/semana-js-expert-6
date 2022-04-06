caf0ddda8608279a306fdbc2948d6ef58807a170
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-04-06 07:15:52 -0300

    Configura eventos dos botões na View

--- public/controller/js/view.js
+++ public/controller/js/view.js
@@ -1,6 +1,16 @@
 export default class View {
+
+  constructor() {
+    this.btnStart = document.getElementById('start')
+    this.btnStop = document.getElementById('stop')
+
+    async function onBtnClick () {}
+    this.onBtnClick = onBtnClick
+  }
+  
   onLoad(){
     this.changeCommandButtonsVisibility();
+    this.btnStart.onclick = this.onStartClicked.bind(this)
   }
 
   changeCommandButtonsVisibility(hide = true){
@@ -12,5 +22,19 @@ export default class View {
       function onClickReset(){}
       button.onclick = onClickReset 
     })
+  }
+
+  configureOnBtnClick(fn) {
+    this.onBtnClick = fn
+  }
+
+  async onStartClicked({
+    srcElement: {
+      innerText
+    }
+  }){
+    const btnText = innerText
+    console.log({ btnText })
+    await this.onBtnClick(btnText)
   }
 }
